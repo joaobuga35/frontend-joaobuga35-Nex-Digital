da3843e3744a3e6d9e8d46acdec1e4c9b97e86ef
joaobuga35 <joaobuga35@gmail.com>
2023-07-29 04:16:12 -0300

    feat: protected route finishing about dashboard

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,13 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import Register from "../pages/Register";
 import Dashboard from "../pages/Dashboard";
 import Login from "../pages/Login";
+import ProtectedRoutes from "../components/ProtectedRoutes";
 
 const RoutesMain = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route element={<ProtectedRoutes />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Route>
     </Routes>
   );
 };
